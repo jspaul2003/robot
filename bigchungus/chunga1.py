58a69a8719bb9b37a4711e304988a12b587a2fbd
jspaul2003 <jspaul2003@gmail.com>
2019-01-23 17:29:01 +0000

    chungus1.02

--- bigchungus/chunga1.py
+++ bigchungus/chunga1.py
@@ -11,11 +11,15 @@ while True:
     if kb.is_pressed('esc'):
         break
     if kb.is_pressed('a'):
-        e.motor.one.forwards()
+        e.motor.one.forwards(100)
         e.motor.two.stop()
     elif kb.is_pressed('d'):
-        e.motor.two.forwards()
+        e.motor.two.forwards(100)
         e.motor.one.stop()
+    elif kb.is_pressed('w'):
+        e.motor.forwards(100)
+    elif kb.is_pressed('s'):
+        e.motor.backwards(100)
     else:
         e.motor.one.stop()
         e.motor.two.stop()
