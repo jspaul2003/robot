5bdac47c1633f0ddd0b22b41664f994ddcc047e5
TheEliteCommando <46781524+TheEliteCommando@users.noreply.github.com>
2019-01-23 17:11:21 +0000

    Update chunga1.py

--- bigchungus/chunga1.py
+++ bigchungus/chunga1.py
@@ -1,4 +1,6 @@
 import explorerhat as e #refers to the explorer hat. we ll be calling it e
+import keyboard as kb
+from time import sleep
 #because java is so good:
 true = True
 false = False
@@ -7,4 +9,15 @@ while true: #go on forever
     i = input('big chungus')
     if i == 'e':
         break
+while True:
+    if kb.is_pressed('a'):
+        e.motor.one.forwards()
+        er.motor.two.stop()
+    elif kb.is_pressed('d')
+        e.motor.two.forwards()
+        e.motor.one.stop()
+    else:
+        e.motor.one.stop()
+        e.motor.two.stop()
+    
 e.motor.stop()
