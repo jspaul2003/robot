c5718f46f80f301c3f02f3a80a9d0c4f81ce38c9
Jean <35814103+jspaul2003@users.noreply.github.com>
2019-01-23 17:01:32 +0000

    BIgChungus1

---
+++ bigchungus/chunga1.py
@@ -0,0 +1,10 @@
+import explorerhat as e #refers to the explorer hat. we ll be calling it e
+#because java is so good:
+true = True
+false = False
+while true: #go on forever
+    e.motor.two.forwards()
+    i = input('big chungus')
+    if i == 'e':
+        break
+e.motor.stop()
